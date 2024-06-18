bb5fc75b2b192985e5d3a9d7364ddcbc53986e0e
James <mackaman.james@gmail.com>
2024-06-17 20:21:27 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       macka                                                        #
+# 	Created:      6/17/2024, 7:43:42 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
